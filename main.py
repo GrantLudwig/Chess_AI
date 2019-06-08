31228d7785a9bf0df6e3295ac2136da1b3931e8d
unknown <grantludwig97@gmail.com>
2019-06-08 15:13:00 -0700

    pawns move correctly

--- main.py
+++ main.py
@@ -81,34 +81,40 @@ def moveList(piece):
                     list.append((currRow - 2, currCol))
         else:
             if allyColor == 'b':
-                if (currRow + 1, currCol) in pieceDict:
-                    nothing = None
-                else:
-                    list.append((currRow + 1, currCol))
+                if currRow + 1 >= 0 and currRow + 1 < 8:
+                    if (currRow + 1, currCol) in pieceDict:
+                        nothing = None
+                    else:
+                        list.append((currRow + 1, currCol))
             else:
-                if (currRow - 1, currCol) in pieceDict:
-                    nothing = None
-                else:
-                    list.append((currRow - 1, currCol))
+                if currRow - 1 >= 0 and currRow - 1 < 8:
+                    if (currRow - 1, currCol) in pieceDict:
+                        nothing = None
+                    else:
+                        list.append((currRow - 1, currCol))
         #attack
         if allyColor == 'b':
             #right
-            if (currRow + 1, currCol + 1) in pieceDict:
-                if pieceDict[(currRow + 1, currCol + 1)].Color != allyColor:
-                    list.append((currRow + 1, currCol + 1))
+            if currCol + 1 >= 0 and currCol + 1 < 8 and currRow + 1 >= 0 and currRow + 1 < 8:
+                if (currRow + 1, currCol + 1) in pieceDict:
+                    if pieceDict[(currRow + 1, currCol + 1)].Color != allyColor:
+                        list.append((currRow + 1, currCol + 1))
             #left
-            if (currRow + 1, currCol - 1) in pieceDict:
-                if pieceDict[(currRow + 1, currCol - 1)].Color != allyColor:
-                    list.append((currRow + 1, currCol - 1))
+            if currCol - 1 >= 0 and currCol - 1 < 8 and currRow + 1 >= 0 and currRow + 1 < 8:
+                if (currRow + 1, currCol - 1) in pieceDict:
+                    if pieceDict[(currRow + 1, currCol - 1)].Color != allyColor:
+                        list.append((currRow + 1, currCol - 1))
         else:
             #right
-            if (currRow - 1, currCol + 1) in pieceDict:
-                if pieceDict[(currRow - 1, currCol + 1)].Color != allyColor:
-                    list.append((currRow - 1, currCol + 1))
+            if currCol - 1 >= 0 and currCol - 1 < 8 and currRow + 1 >= 0 and currRow + 1 < 8:
+                if (currRow - 1, currCol + 1) in pieceDict:
+                    if pieceDict[(currRow - 1, currCol + 1)].Color != allyColor:
+                        list.append((currRow - 1, currCol + 1))
             #left
-            if (currRow - 1, currCol - 1) in pieceDict:
-                if pieceDict[(currRow - 1, currCol - 1)].Color != allyColor:
-                    list.append((currRow - 1, currCol - 1))
+            if currCol - 1 >= 0 and currCol - 1 < 8 and currRow - 1 >= 0 and currRow - 1 < 8:
+                if (currRow - 1, currCol - 1) in pieceDict:
+                    if pieceDict[(currRow - 1, currCol - 1)].Color != allyColor:
+                        list.append((currRow - 1, currCol - 1))
         return list
     #rook
     elif piece.Type == 'r':
